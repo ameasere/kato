f79e5b920902e71d346ff869d35ecef40bb20fb2
Leigh <leigh@ameasere.com>
2024-02-11 21:04:07 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
-    "sphinx.ext.intersphinx"
+    "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
